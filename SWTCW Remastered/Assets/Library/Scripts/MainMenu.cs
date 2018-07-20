c9710fdbfdd9134201cb9d1d8f4817d611d0a987
Cameron Smith <camerosmit@yahoo.com>
2018-07-20 09:51:31 -0700

    Fixed a lighting bug on the MainMenu and setup the credits screen.

--- SWTCW Remastered/Assets/Library/Scripts/MainMenu.cs
+++ SWTCW Remastered/Assets/Library/Scripts/MainMenu.cs
@@ -7,12 +7,20 @@ public class MainMenu : MonoBehaviour {
 	public Animator cam;
 	public GameObject startText;
 	public GameObject mainMenu;
+	public GameObject optionsMenu;
+	public GameObject credits;
+	private bool bCreditsRolling;
 
 	private void OnGUI()
 	{
 		if (Input.GetButtonDown("Start"))
 		{
 			StartCoroutine(TransitionToMainMenu());
+		}
+
+		if(bCreditsRolling && (Input.GetButtonDown("Cancel")))
+		{
+			StopCredits();
 		}
 	}
 
@@ -22,5 +30,19 @@ public class MainMenu : MonoBehaviour {
 		yield return new WaitForSeconds(cam.GetCurrentAnimatorStateInfo(0).length);
 		startText.SetActive(false);
 		mainMenu.SetActive(true);
+	}
+
+	public void RollCredits(bool choice)
+	{
+		bCreditsRolling = choice;
+		Cursor.visible = false;
+	}
+
+	public void StopCredits()
+	{
+		Cursor.visible = true;
+		credits.SetActive(false);
+		optionsMenu.SetActive(true);
+		bCreditsRolling = false;
 	}
 }
